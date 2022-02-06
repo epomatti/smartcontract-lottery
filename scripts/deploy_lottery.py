103c1a68032fd766f6e24db436a455463be33cda
epomatti <evandro.pomatti@gmail.com>
2022-02-06 17:03:33 -0300

    start, enter, and end lottery

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,5 +1,6 @@
 from brownie import Lottery, network, config
-from scripts.utils import get_account, get_contract
+from scripts.utils import get_account, get_contract, fund_with_link
+import time
 
 
 def deploy_lottery():
@@ -24,6 +25,28 @@ def start_lottery():
     print("Lottery started!")
 
 
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(f"{lottery.recentWinner()} is the new winner!")
+
+
 def main():
     deploy_lottery()
     start_lottery()
+    enter_lottery()
+    end_lottery()
